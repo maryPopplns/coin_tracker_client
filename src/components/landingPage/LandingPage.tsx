1caa544224fff5b1c1196066e0b4aa41759c88ff
Spencer Knight <spencer.knightusn@gmail.com>
2022-06-09 14:48:55 -0700

    created landing page navbar component

--- src/components/landingPage/LandingPage.tsx
+++ src/components/landingPage/LandingPage.tsx
@@ -1,26 +1,19 @@
 import './landingPage.css';
 import React, { useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
+import LandingPageNavbar from '../landingPageNavbar/LandingPageNavbar';
 
 import bitcoin from './assets/Bitcoin.png';
 import ethereum from './assets/Ethereum.png';
 import dashboard from './assets/Dashboard.png';
-import brandIcon from './assets/Brand-icon.png';
 
-// TODO change top calculations when dashboard-wrapper size is complete
+// TODO change the css selector for signup anchor tag (showing incorrectly on gh)
+// TODO create module for navbar
 
 function LandingPage(): JSX.Element {
   return (
     <main className='landing-page'>
-      <header>
-        <Link to='/'>
-          <img className='brand-icon' src={brandIcon} alt='brand icon' />
-        </Link>
-        <div>
-          <Link to='/signup'>sign up</Link>
-          <Link to='/login'>login</Link>
-        </div>
-      </header>
+      <LandingPageNavbar />
       <h1 className='landing-page-heading'>Coin Tracker</h1>
       <p className='landing-page-description'>
         State of the art technologies to track your defi portfolio
---
+++ src/components/landingPageNavbar/LandingPageNavbar.tsx
@@ -0,0 +1,20 @@
+import './landingPageNavbar.css';
+import { Link } from 'react-router-dom';
+
+import brandIcon from './assets/Brand-icon.png';
+
+function LandingPageNavbar(): JSX.Element {
+  return (
+    <header>
+      <Link to='/'>
+        <img className='brand-icon' src={brandIcon} alt='brand icon' />
+      </Link>
+      <div>
+        <Link to='/signup'>sign up</Link>
+        <Link to='/login'>login</Link>
+      </div>
+    </header>
+  );
+}
+
+export default LandingPageNavbar;
