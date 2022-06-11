f05dc3379f2628b511e1e1a99dfe9106aa8e6fa7
Spencer Knight <spencer.knightusn@gmail.com>
2022-06-10 19:20:15 -0700

    added signup header, labels, and inputs.

--- src/components/signup/Signup.tsx
+++ src/components/signup/Signup.tsx
@@ -5,6 +5,25 @@ function Signup(): JSX.Element {
   return (
     <main className='signup-page'>
       <LandingPageNavbar />
+      <form className='signup-form'>
+        <h2>Sign Up</h2>
+        <div>
+          <label htmlFor='signup-username'>username</label>
+          <input
+            type='text'
+            name='signup-username'
+            id='signup-username'
+          ></input>
+        </div>
+        <div>
+          <label htmlFor='signup-password'>password</label>
+          <input
+            type='text'
+            name='signup-password'
+            id='signup-password'
+          ></input>
+        </div>
+      </form>
     </main>
   );
 }
