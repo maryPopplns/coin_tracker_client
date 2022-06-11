348eea3e427efddda10672229941311e7324840a
Spencer Knight <spencer.knightusn@gmail.com>
2022-06-10 20:18:24 -0700

    sign up form styling and positioning complete

--- src/components/signup/Signup.tsx
+++ src/components/signup/Signup.tsx
@@ -5,25 +5,27 @@ function Signup(): JSX.Element {
   return (
     <main className='signup-page'>
       <LandingPageNavbar />
-      <form className='signup-form'>
-        <h2>Sign Up</h2>
-        <div>
-          <label htmlFor='signup-username'>username</label>
-          <input
-            type='text'
-            name='signup-username'
-            id='signup-username'
-          ></input>
-        </div>
-        <div>
-          <label htmlFor='signup-password'>password</label>
-          <input
-            type='text'
-            name='signup-password'
-            id='signup-password'
-          ></input>
-        </div>
-      </form>
+      <div className='signup-form-container'>
+        <form className='signup-form'>
+          <h2>Sign Up</h2>
+          <div className='signup-input-container'>
+            <label htmlFor='signup-username'>username</label>
+            <input
+              type='text'
+              name='signup-username'
+              id='signup-username'
+            ></input>
+          </div>
+          <div className='signup-input-container'>
+            <label htmlFor='signup-password'>password</label>
+            <input
+              type='text'
+              name='signup-password'
+              id='signup-password'
+            ></input>
+          </div>
+        </form>
+      </div>
     </main>
   );
 }
